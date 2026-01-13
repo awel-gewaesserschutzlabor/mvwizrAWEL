4ff02b7639dbde8062a8c5196789880db2c6cc43
awel-gewaesserschutzlabor <michelle.salvisberg@bd.zh.ch>
2026-01-13 19:54:24 +0100

    Implement mvwizr version check on package attach
    Added version check and warning for mvwizr package if installed version of mvwizr is not the same as the most recent version on github

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,24 @@
-.onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("remotes", quietly = TRUE)) {
-    install.packages("remotes")
+.onAttach <- function(libname, pkgname) {
+
+  # prüft installierte mvwizr version
+  installed <- utils::packageVersion("mvwizr")
+
+  # github version abrufen
+  url <- "https://api.github.com/repos/ror-at-ebp/mvwizr/releases/latest"
+  res <- try(httr::GET(url), silent = TRUE)
+
+  if (inherits(res, "try-error")||res$status_code != 200){ # warnmeldung falls github nicht erreicht
+    warning("Github konnte nicht erreicht werden. Bitte überprüfe deine Internetverbindung")
   }
-  # beim Laden von mvwizrAWEL mvwizr immer auf neueste Version updaten
-  try({
-    remotes::install_github("ror-at-ebp/mvwizr", upgrade = "always")
-  }, silent = TRUE)
+
+  latest_tag <- jsonlite::fromJSON(httr::content(res, as = "text", encoding = "UTF-8"))$tag_name # sucht nach neuster version auf github
+  latest <- numeric_version(gsub("v\\s*", "", latest_tag)) # sauberes Versionsobjekt erzeugen
+
+  # vergleicht github version mit installierter Version
+  if (installed < latest){
+    warning(sprintf("Deine mvwizr Version (%s) stimmt nicht mit der neusten GitHub Version (%s) überein. Bitte aktualisiere mvwizr.",
+                    installed, latest))
+  } else {
+    packageStartupMessage("Du hast die neuste Version von mvwizr (%s)", installed)
+  }
 }
